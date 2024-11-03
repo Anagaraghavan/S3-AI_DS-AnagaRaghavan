915a2794f6d16641ae88e1286213eab8de85fe19
Anagaraghavan <anagaraghavan04@gmail.com>
2024-11-03 19:32:47 +0530

    Update linearsearch.c

--- lab/EXP3_Linearsearch/linearsearch.c
+++ lab/EXP3_Linearsearch/linearsearch.c
@@ -1,1 +1,30 @@
-
+//Linear Search
+//Anaga Raghavan
+//Roll No:8
+#include<stdio.h>
+int main() {
+	int a[50],search,i,found=0,num;
+	printf("Enter the number of elements:");
+	scanf("%d",&num);
+	printf("Enter the elements in array:");
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter the element to search:");
+	scanf("%d",&search);
+	for(i=0;i<num;i++){
+	if(a[i]==search){
+		found = 1;
+		break;
+	}
+	}
+	if(found)
+	{
+		printf("The element was found at position %d\n",i+1);
+	}
+	else{
+		printf("The element was not found\n");
+	}
+	return 0;
+}
